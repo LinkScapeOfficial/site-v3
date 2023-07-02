2e73f6761544cbce19e5a24bc06e2cc2b46e48ac
Thomas Wu <thomaswcy@hotmail.com>
2023-07-02 12:51:20 +0800

    something

--- components/leaders.tsx
+++ components/leaders.tsx
@@ -36,29 +36,34 @@ const people = [
 
 export default function Leaders() {
     return (
-        <div className="bg-white mb-10 -mt-20">
-            <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
-                <div className="max-w-2xl">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet Our Team</h2>
+      <div className="bg-white mb-10 -mt-5">
+        <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-10 sm:gap-y-12 px-6 lg:px-8 xl:grid-cols-3">
+          <div className="max-w-2xl">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl -mb-6">Meet Our Team</h2>
+          </div>
+          <ul role="list" className="grid gap-x-8 gap-y-6 sm:grid-cols-2 sm:gap-y-8 xl:col-span-2">
+            {people.map((person) => (
+              <li key={person.name}>
+                <div className="flex items-center gap-x-6">
+                  <img className="h-16 w-16 rounded-full" src={person.imageUrl} alt="" />
+                  <div>
+                    <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
+                    <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
+                  </div>
                 </div>
-                <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
-                    {people.map((person) => (
-                        <li key={person.name}>
-                            <div className="flex items-center gap-x-6">
-                                <img className="h-16 w-16 rounded-full" src={person.imageUrl} alt="" />
-                                <div>
-                                    <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
-                                    <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
-                                </div>
-                            </div>
-                        </li>
-                    ))}
-                    <Link className="flex items-center gap-x-6" href="/team">
-                        <FiMoreHorizontal className="h-16 w-16 rounded-full" />
-                        <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900 -mt-1">View More</h3>
-                    </Link>
-                </ul>
-            </div>
+              </li>
+            ))}
+            <li className="flex items-center gap-x-6">
+              <Link href="/team">
+                <div className="flex items-center gap-x-6">
+                  <FiMoreHorizontal className="h-16 w-16 rounded-full" />
+                  <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900 -mt-1">View More</h3>
+                </div>
+              </Link>
+            </li>
+          </ul>
         </div>
+      </div>
     )
-}
+  }
+  
