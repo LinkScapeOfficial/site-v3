2b529c9ce85fddd2948a2e28396e47079ffe5c3a
Evan Luo <contact@ohevan.com>
2023-08-04 22:51:07 -0400

    goodbye blur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,13 +16,13 @@ export default function Index() {
     <>
       <NavBar />
       <div className="mt-12 flex flex-col items-center justify-center min-h-screen">
-        <div
-          className="absolute top-1/3 translate-y-[20px] sm:-translate-y-[40px] inset-0 grid grid-cols-2 -space-x-12 opacity-40 dark:opacity-20 sm:-space-x-52"
-          style={{ zIndex: -1 }}
-        >
-          <div className="fix-safari-blur h-48 bg-gradient-to-br from-blue-500/60 to-blue-400/60 blur-[80px] dark:from-blue-700 sm:h-64 sm:blur-[120px]"></div>
-          <div className="fix-safari-blur h-32 bg-gradient-to-r from-blue-400/60 to-blue-300/60 blur-[80px] dark:to-blue-600 sm:h-40 sm:blur-[120px]"></div>
-        </div>
+        {/*<div*/}
+        {/*  className="absolute top-1/3 translate-y-[20px] sm:-translate-y-[40px] inset-0 grid grid-cols-2 -space-x-12 opacity-40 dark:opacity-20 sm:-space-x-52"*/}
+        {/*  style={{ zIndex: -1 }}*/}
+        {/*>*/}
+        {/*  <div className="fix-safari-blur h-48 bg-gradient-to-br from-blue-500/60 to-blue-400/60 blur-[80px] dark:from-blue-700 sm:h-72 sm:blur-[120px]"></div>*/}
+        {/*  <div className="fix-safari-blur h-32 bg-gradient-to-r from-blue-400/60 to-blue-300/60 blur-[80px] dark:to-blue-600 sm:h-40 sm:blur-[120px]"></div>*/}
+        {/*</div>*/}
         <HomeTitle />
         <HomeButtons />
       </div>
