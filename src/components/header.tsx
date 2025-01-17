df105ce1a8e3d27d774125cdced8f80aed4de767
Evan Luo <contact@ohevan.com>
2025-01-17 15:47:26 -0500

    refactor: remove commented-out mobile navigation code from header component
    - Eliminated unused code for mobile navigation in the header component to improve readability and maintainability.
    - Streamlined the header structure by removing unnecessary comments, enhancing overall code clarity.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -180,27 +180,6 @@ export default function Header() {
           </div>
         </div>
       </div>
-
-      {/* {open && (
-        <div className="sm:hidden">
-          <div className="space-y-1 px-2 pb-3 pt-2">
-            {navigation.map((item) => (
-              <Link
-                key={item.name}
-                href={item.href}
-                className={`${
-                  item.current
-                    ? "bg-gray-900 text-white"
-                    : "text-black hover:bg-blue-500 hover:text-white"
-                } block rounded-md px-3 py-2 text-base font-medium`}
-                aria-current={item.current ? "page" : undefined}
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div>
-        </div>
-      )} */}
     </header>
   );
 }
