922d2acaa81b581767c64568514e25fee6d86438
Evan Luo <contact@ohevan.com>
2025-01-16 23:15:58 -0500

    chore: update dependencies and enhance Tailwind CSS configuration
    - Added new dependencies: class-variance-authority, clsx, geist, tailwind-merge, and tailwindcss-animate.
    - Updated Tailwind CSS configuration to include new color and background utilities.
    - Refactored global styles to incorporate CSS variables for theming.
    - Simplified animations in HomeButtons and HomeTitle components by removing framer-motion.
    - Adjusted layout and styling for improved responsiveness and visual consistency.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+
 import { Analytics } from "@vercel/analytics/react";
 import Footer from "@/components/footer";
 
@@ -34,12 +37,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div
+      <body className={`${GeistSans.variable} ${GeistMono.variable} font-sans`}>
+        {/* <div
           className={
             "background-dotted w-screen fixed left-0 top-0 h-screen z-[-1] dark:opacity-10"
           }
-        />
+        /> */}
         {children}
 
         <Analytics />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,13 @@ export default function Index() {
   return (
     <>
       <NavBar />
-      <div
+      {/* <div
         className="absolute inset-0 grid grid-cols-2 -space-x-12 opacity-40 dark:opacity-20 sm:-space-x-52"
         style={{ zIndex: -1 }}
       >
         <div className="fix-safari-blur h-32 bg-gradient-to-br from-blue-500 to-blue-400 blur-[32px] dark:from-blue-700 sm:h-64 sm:blur-[106px]"></div>
         <div className="fix-safari-blur h-20 bg-gradient-to-r from-blue-400 to-blue-300 blur-[32px] dark:to-blue-600 sm:h-40 sm:blur-[106px]"></div>
-      </div>
+      </div> */}
       <div className="mt-6 flex flex-col items-center justify-center min-h-screen">
         <HomeTitle />
         <HomeButtons />
