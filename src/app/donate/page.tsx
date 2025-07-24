5b4bb1d26d957acbe34500584117ec5c98f75bc3
Evan Luo <contact@ohevan.com>
2025-07-24 17:25:46 -0400

    chore: update development script and refresh sitemap with new last modified dates

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -11,12 +11,12 @@ export default function Donate() {
   return (
     <>
       <NavBar />
-      <div className="h-512px screen flex flex-col mt-16">
+      <div className="h-512px screen flex flex-col mt-16 linkscape-wrapper">
         <iframe
-          src="https://bank.hackclub.com/donations/start/linkscape"
+          src="https://hcb.hackclub.com/donations/start/linkscape"
           name="donateFrame"
           height="780px"
-          width="screen"
+          width="100%"
           allowFullScreen
         ></iframe>
       </div>
